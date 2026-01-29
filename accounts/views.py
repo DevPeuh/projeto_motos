557094cf01b823e57667dca0e56b18352fb198b9
DevPeuh <phgdealmeida@gmail.com>
2026-01-29 14:15:19 -0300

    Authentication-user for detail bike

--- accounts/views.py
+++ accounts/views.py
@@ -18,7 +18,7 @@ def login_view(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user is not None: 
             login(request, user)
             return redirect('bikes_list')  
         else:
