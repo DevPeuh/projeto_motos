c684bc599c2447354cf91de490db6ea6d320bbed
DevPeuh <phgdealmeida@gmail.com>
2026-02-02 14:18:07 -0300

    Auth final

--- bikes/views.py
+++ bikes/views.py
@@ -1,6 +1,8 @@
 from bikes.models import Bike
 from .forms import BikeModelForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
@@ -16,18 +18,19 @@ class BikesListView(ListView):
         if search:
             bike = bike.filter(model__icontains=search) #icontains é para ignorar maiúsculas ou minúsculas
         return bike
-    
+
+@method_decorator(login_required(login_url='/login/'), name='dispatch') # dispatch é o método que lida com as requisições HTTP
 class NewBikeCreateView(CreateView):
     model = Bike
     form_class = BikeModelForm
     template_name = 'new_bike.html'
     success_url = '/bikes/'
 
- 
 class BikeDetailView(DetailView):
     model = Bike
     template_name = 'bike_detail.html'
 
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
 class BikeUpdateView(UpdateView):
     model = Bike
     form_class = BikeModelForm
@@ -35,7 +38,8 @@ class BikeUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse_lazy('bike_detail', kwargs={'pk': self.object.pk})
-
+    
+@method_decorator(login_required(login_url='/login/'), name='dispatch')
 class BikeDeleteView(DeleteView):
     model = Bike
     template_name = 'bike_delete.html'
