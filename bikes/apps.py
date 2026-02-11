f09cf61882364765e80ac7d95d120c03bcbeea5a
DevPeuh <phgdealmeida@gmail.com>
2026-02-11 16:02:25 -0300

    add signals

--- bikes/apps.py
+++ bikes/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BikesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bikes'
+
+    def ready(self):
+        import bikes.signals
